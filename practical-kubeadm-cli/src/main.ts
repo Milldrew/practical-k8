4cb99c9b7e2b12627581afd6222740c369833f4c
Andrew Miller <Andrew.patrick.miller.2021@gmail.com>
2025-07-24 16:12:55 -0600

    📅  | Thu Jul 24 16:12:55 MDT 2025

--- practical-kubeadm-cli/src/main.ts
+++ practical-kubeadm-cli/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppController } from './app.controller';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const appController = app.get(AppController);
+
+  console.log(appController.getHello());
 }
 bootstrap();
